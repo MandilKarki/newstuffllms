5b1a2f5882c890e4b5b2d55dd298e3103ee89992
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-06-06 01:58:25 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,77 @@
+    Example 1:
+        Email Content: "Hello team, I have discovered a security vulnerability in our network. It's crucial that we address this issue promptly. I will discuss the details during our meeting tomorrow."
+        Classification: Not Risky
+        Reason: The email is classified as not risky as it pertains to the identification of a security vulnerability within the organization's network. It emphasizes the need for prompt action and discussion during a scheduled meeting, suggesting responsible handling of the issue.
+
+    Example 2:
+        Email Content: "URGENT: Your account has been compromised. Click the link below to reset your password immediately to prevent unauthorized access."
+        Classification: Risky
+        Reason: The email is classified as risky due to the urgent nature and the request to click on a link to reset the password. Such urgent requests and the inclusion of clickable links without proper authentication are common tactics used in phishing attempts, potentially leading to unauthorized access to sensitive information.
+
+    Example 3:
+        Email Content: "Hi John, I've attached the new product roadmap. Please review it and let me know your thoughts. We need to keep this information confidential until it's officially announced."
+        Classification: Not Risky
+        Reason: The email is classified as not risky because it involves the authorized sharing of the new product roadmap with a reminder to keep the information confidential until the official announcement. It demonstrates responsible communication and compliance with confidentiality requirements.
+
+    Example 4:
+        Email Content: "Attention all employees: We are conducting a cybersecurity training session next week. It's important that everyone attends to stay updated on the latest threats and preventive measures."
+        Classification: Not Risky
+        Reason: The email is classified as not risky as it provides essential information about a scheduled cybersecurity training session for all employees. It highlights the importance of staying updated on the latest threats and preventive measures, promoting security awareness within the organization.
+
+    Example 5:
+        Email Content: "Hey, I found an interesting loophole in our security system. I think it's worth investigating further. Let's schedule a private meeting to discuss this matter."
+        Classification: Risky
+        Reason: The email is classified as risky due to the mention of a potential loophole in the security system and the suggestion to schedule a private meeting. This email raises concerns as it may indicate an intent to exploit or manipulate security vulnerabilities without proper authorization or oversight.
+
+These examples provide more subtle scenarios where the classification depends on the context, intent, and potential risks associated with the email content. The reasons for the classification help explain why the email is deemed risky or not risky based on the presence of indicators of insider threats, policy compliance, and data handling considerations.
+User
+and for this
+    Example 1:
+        Email Content: "Dear Team, Please find attached the financial report for this quarter. It contains confidential information. Make sure to handle it securely."
+        Classification: Not Risky
+
+    Example 2:
+        Email Content: "URGENT: Immediate action required! Reply with your login credentials to resolve the account verification issue."
+        Classification: Risky
+
+    Example 3:
+        Email Content: "Hi John, I have attached the customer database for your review. Please ensure you follow the established data protection policies when accessing it."
+        Classification: Not Risky
+
+    Example 4:
+        Email Content: "Attention all employees: The new security measures for access control are now in effect. Please review the attached document and comply accordingly."
+        Classification: Not Risky
+
+    Example 5:
+        Email Content: "Hey, I found an interesting vulnerability in our system. Let's discuss it privately. Don't share this information with anyone else."
+        Classification: Risky
+ChatGPT
+
+Here are the updated few-shot examples with the classification labels and the reasons for considering the emails as risky or not risky:
+
+    Example 1:
+        Email Content: "Dear Team, Please find attached the financial report for this quarter. It contains confidential information. Make sure to handle it securely."
+        Classification: Not Risky
+        Reason: The email is classified as not risky as it involves the authorized sharing of a financial report within the team, emphasizing the confidential nature of the information and the need for secure handling.
+
+    Example 2:
+        Email Content: "URGENT: Immediate action required! Reply with your login credentials to resolve the account verification issue."
+        Classification: Risky
+        Reason: The email is classified as risky due to the presence of urgency, the request for login credentials, and the potential for a phishing attempt. Such requests for sensitive information should be treated with caution to avoid unauthorized access.
+
+    Example 3:
+        Email Content: "Hi John, I have attached the customer database for your review. Please ensure you follow the established data protection policies when accessing it."
+        Classification: Not Risky
+        Reason: The email is classified as not risky as it involves the authorized sharing of the customer database with a reminder to adhere to data protection policies. It promotes responsible data handling within the organization.
+
+    Example 4:
+        Email Content: "Attention all employees: The new security measures for access control are now in effect. Please review the attached document and comply accordingly."
+        Classification: Not Risky
+        Reason: The email is classified as not risky as it provides important information regarding new security measures to all employees, emphasizing the need for compliance. It promotes security awareness and adherence to access control protocols.
+
+    Example 5:
+        Email Content: "Hey, I found an interesting vulnerability in our system. Let's discuss it privately. Don't share this information with anyone else."
+        Classification: Risky
+        Reason: The email is classified as risky due to the mention of a system vulnerability and the request to discuss it privately. Such discussions should be carried out within proper channels and with appropriate authorization to ensure security and prevent potential misuse or unauthorized disclosure.
+
+These examples provide additional instances where the classification depends on the context, intent, and potential risks associated with the email content. The reasons for the classification help explain why the email is deemed risky or not risky based on indicators of insider threats, policy compliance, and data handling considerations.
