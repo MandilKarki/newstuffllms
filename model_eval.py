ddf3e2a57562e3ec6c4416086b7eb00a09663546
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-06-12 20:44:05 -0400

    Update model_eval.py

--- model_eval.py
+++ model_eval.py
@@ -75,3 +75,76 @@ if __name__ == "__main__":
     ]
 
     main(csv_file, output_file)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    import re
+import pandas as pd
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from pathlib import Path
+
+model_id = Path("C:/LLM/quantised_bloom/")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto", llm_int8_enable_fp32_cpu_offload=True)
+pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, torch_dtype=torch.bfloat16, trust_remote_code=True, device_map="auto",)
+disclaimer_starts = ["This email", "This e-mail", "This mail is intended", "This email is intended", "Confidential", "Privileged", "Property"]
+
+disclaimer_patterns = [re.compile(re.escape(start), flags=re.S | re.I) for start in disclaimer_starts]
+
+def remove_disclaimer(email):
+    for pattern in disclaimer_patterns:
+        match = pattern.match(email)
+        if match:
+            return match.group(1)
+    return email
+
+def preprocess_email(email):
+    email = remove_disclaimer(email)
+    return email.replace("\n", " ")
+
+def classify_email(email):
+    sequences = pipeline(
+        f'''Below is an instruction that describes a task. Write a response that appropriately completes the request.
+        ### Instruction: The task is to just output a classification label and stop and not generate any more
+        text based on potential indicators of insider threats. the output labels can be either risky or non-risky. Do not add any more text!!
+        you will leverage the power of language understanding to accurately identify suspicious emails and trigger appropriate actions to mitigate risks.
+        .
+        .
+        . # Rest of the text removed for brevity
+        Based on all of this context. Classify this Email Content as Risky or Not risky:
+        Email Content:
+        {email}
+        Classification:
+        ''',
+        max_length=1015,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+
+    generated_text = sequences[0]["generated_text"]
+    prompt_tokens = tokenizer.encode(email, add_special_tokens=False)
+    generated_tokens = tokenizer.encode(generated_text, add_special_tokens=False)
+    generated_text = tokenizer.decode(generated_tokens[len(prompt_tokens):])
+    return generated_text.strip()
+
+def main(csv_file, output_file):
+    df = pd.read_csv(csv_file)
+    df['email'] = df['email'].apply(preprocess_email)
+    df['classification'] = df['email'].apply(classify_email)
+    df.to_csv(output_file, index=False)
+
+if __name__ == "__main__":
+    csv_file = "input.csv"  
+    output_file = "output_data.csv"  
+    main(csv_file, output_file)
+
