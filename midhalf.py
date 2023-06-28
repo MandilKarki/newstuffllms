26c2c82a7c875ea15cede6c65382c8701294a94d
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-06-28 11:43:53 -0400

    Update midhalf.py

--- midhalf.py
+++ midhalf.py
@@ -49,4 +49,47 @@ def process_emails(filename):
     return df
 
 df = process_emails('emails.csv')
-print(df)
+print(df
+
+
+import pandas as pd
+import re
+
+def clean_email(text):
+    # Define patterns to remove
+    patterns = [
+        r'From: .*',  # Remove 'From:' lines
+        r'To: .*',  # Remove 'To:' lines
+        r'Sent: .*',  # Remove 'Sent:' lines
+        r'Subject: .*',  # Remove 'Subject:' lines
+        r'cc: .*',  # Remove 'cc:' lines
+        r'\[.*\.(jpg|png|gif)\]',  # Remove bracket enclosed image file references
+        r'www\..*\.com',  # Remove URLs
+        r'http.*',  # Remove URLs
+        r'_+',  # Remove lengthy underscore dividers
+        r'This email was sent by.*',  # Remove common disclaimer
+        r'Confidentiality Notice.*',  # Remove common disclaimer
+        r'\w+@\w+\.\w+',  # Remove email addresses
+        r'If you received this email in error.*',  # Remove common disclaimer
+        r'Ce courriel a.*',  # Remove French version of the disclaimer
+    ]
+
+    cleaned_text = text
+    for pattern in patterns:
+        cleaned_text = re.sub(pattern, '', cleaned_text, flags=re.MULTILINE|re.IGNORECASE|re.DOTALL)
+    
+    # Remove multiple empty lines
+    cleaned_text = re.sub(r'\n{3,}', '\n\n', cleaned_text)
+    
+    return cleaned_text.strip()
+
+# Example usage with a DataFrame 'df' containing a column 'body'
+df = pd.DataFrame({
+    'body': [
+        '''<Your example email text here>''',
+        # More emails...
+    ]
+})
+
+df['cleaned_body'] = df['body'].apply(clean_email)
+
