77736a3174d284ed6c2024b8128b08ce0766c798
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-06-28 10:11:18 -0400

    Create midhalf.py

---
+++ midhalf.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from fuzzywuzzy import fuzz
+
+def process_emails(filename):
+    df = pd.read_csv(filename)
+
+    df['self_sent'] = df.apply(lambda row: fuzz.ratio(row['sender'], row['receivers']) > 80, axis=1)
+
+    df['num_receivers'] = df['receivers'].apply(lambda x: len(x.split(';')))
+
+    df['indicator'] = df.apply(lambda row: 'self' if row['self_sent'] and row['num_receivers']==1 else 'multi' if row['num_receivers']>10 else 'normal', axis=1)
+
+    return df
+
+df = process_emails('emails.csv')
+print(df)
