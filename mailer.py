f7967af28a8f78195a17f69ba949efe8e23f2ab7
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-08-18 11:27:31 -0400

    Create mailer.py

---
+++ mailer.py
@@ -0,0 +1,11 @@
+Hi Karleigh,
+
+I trust you've been doing well. It's been a while since our last conversation, and I was eager to touch base.
+
+I'm approaching a significant milestone: the conclusion of my co-op term and my graduation. With this transition, I've been gravitating towards opportunities in Data Science, AI, Machine Learning, and Data Engineering. I believe these fields resonate deeply with my passion and academic learnings.
+
+Karleigh, do you happen to know of any roles or teams that might be looking for someone with my skill set and enthusiasm? Your insights, given your extensive experience, would be invaluable. If you're aware of any openings or professionals in this realm I could speak to, I'd be incredibly grateful.
+
+Hoping to hear your thoughts and recommendations. Take care!
+
+Warmly,
