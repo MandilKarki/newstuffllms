73524b67822b1fa16bbfaf0595e5bfc31f4d8d6e
MandilKarki <71919875+MandilKarki@users.noreply.github.com>
2023-08-21 12:51:06 -0400

    Update final_week_attachment_bone.py

--- final_week_attachment_bone.py
+++ final_week_attachment_bone.py
@@ -1,3 +1,118 @@
+import pandas as pd
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import transformers
+
+# Loading model weights directory
+model_id = "C:/LLM/wizardvicuna"
+
+# Loading model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(model_id)
+
+# Initializing the model pipeline
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    device_map="auto",
+)
+print("pipeline initiated")
+
+
+def text_splitter(text):
+    return text[:1950] if isinstance(text, str) else ' '
+
+
+def load_data(csv_file):
+    return pd.read_csv(csv_file)
+
+
+def preprocess_email(email):
+    return text_splitter(email).replace("\n", " ")
+
+
+def generate_response(prompt):
+    sequences = pipeline(
+        text_inputs=prompt,
+        max_length=len(prompt) + 100,  # Increased to accommodate the additional possible outputs.
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    return sequences[0]["generated_text"]
+
+
+def classify_email(email, subject, attachments, sender):
+    # Analyze Subject:
+    subject_result = generate_response(f"Given the email subject '{subject}', determine if there are any indicators of sensitive or client-specific data being present.").split()[-1]
+
+    # Analyze Body:
+    body_result = generate_response(f"With the understanding that the subject indicates '{subject_result}', review the body '{email}' for signs of data exfiltration or unauthorized handling.").split()[-1]
+
+    # Analyze Attachments:
+    attachment_result = generate_response(f"Given the subject's nature as '{subject_result}' and the body's analysis as '{body_result}', inspect the attachments {attachments} for any evidence of client data or malicious intent.").split()[-1]
+
+    # Final Classification:
+    final_classification_prompt = f"Consolidating the analysis: subject ({subject_result}), body ({body_result}), and attachments ({attachment_result}), provide a final classification of the email as 'Suspicious' or 'Not Suspicious'."
+    final_classification = generate_response(final_classification_prompt).split("classification:")[-1].strip()
+
+    return final_classification
+
+
+def generate_output(df):
+    df["classification"] = df.apply(
+        lambda row: classify_email(row["cleaned_body"], row["messageSubject"], row["attachments"], row["networkSenderIdentifier"]),
+        axis=1
+    )
+    return df
+
+
+def save_results(df, output_file):
+    df.to_csv(output_file, index=False)
+
+
+def main(csv_file, output_file):
+    df = load_data(csv_file)
+    print("data loaded")
+    print(df)
+    df = generate_output(df)
+    save_results(df, output_file)
+
+
+if __name__ == "__main__":
+    csv_file = "C:\LLM\August\A16august.csv"
+    output_file = "resultsAugust16.csv"
+    main(csv_file, output_file)
+
+
+
+
+
+
+Analyze Subject:
+        Prompt: "Given the email subject {subject}, determine if there are any indicators of sensitive or client-specific data being present."
+        Possible Outputs: 'Client Data', 'Sensitive', 'Neutral', 'Unsure'
+
+    Analyze Body:
+        Use the output from step 1.
+        Prompt: "With the understanding that the subject indicates {outputFromStep1}, review the body {email} for signs of data exfiltration or unauthorized handling."
+        Possible Outputs: 'Definitely Suspicious', 'Potentially Suspicious', 'Not Suspicious', 'Unsure'
+
+    Analyze Attachments:
+        Use outputs from steps 1 & 2.
+        Prompt: "Given the subject's nature as {outputFromStep1} and the body's analysis as {outputFromStep2}, inspect the attachments {attachments} for any evidence of client data or malicious intent."
+        Possible Outputs: 'High Risk', 'Moderate Risk', 'Low Risk', 'No Risk'
+
+    Final Classification:
+        Use outputs from steps 1, 2 & 3.
+        Prompt: "Consolidating the analysis: subject ({outputFromStep1}), body ({outputFromStep2}), and attachments ({outputFromStep3}), provide a final classification of the email as 'Suspicious' or 'Not Suspicious'."
+
+
+
+
+
+
 Case 1: Email sent to oneself with client data
 
 Prompt: "Analyze an email sent to {selfEmail} containing the subject {subject}, body {email}, and attachments {attachments}. Given that this email is sent to oneself and contains potential client data, determine if there's evidence of insider threats or unauthorized data handling."
